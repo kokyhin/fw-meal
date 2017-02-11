2a6d3e34f2fe79847a6387a021b877a29e8bb2e8
kokyhin <kokyhin@gmail.com>
2017-02-11 15:26:53 +0200

    Added data table

--- routes/order.js
+++ routes/order.js
@@ -9,6 +9,97 @@ ensureAuthenticated = function(req, res, next) {
     res.status(401).send({error: err.message});
   }
 };
+function getMonday(d) {
+  d = new Date(d);
+  if(d.getDay() == 5 && d.getHours() > 13) {
+    d.setDate(d.getDate() + 3);
+    var day = d.getDay(),
+        diff = d.getDate() - day + 1;
+    return new Date(d.setDate(diff));
+  } else if (d.getDay() > 5) {
+    d.setDate(d.getDate() + 3);
+    var day = d.getDay(),
+        diff = d.getDate() - day + 1;
+    return new Date(d.setDate(diff));
+  } else {
+    var day = d.getDay(),
+        diff = d.getDate() - day + 1; // adjust when day is sunday
+    return new Date(d.setDate(diff));
+  }
+}
+var day = getMonday(new Date());
+var week = [
+  {
+    name: 'Пн',
+    day: day.getDate(),
+    active: false,
+    dayid: day.getDate() + '' + day.getMonth() + '' + day.getFullYear(),
+    first: 0,
+    second: 0,
+    full: 0,
+    price: {
+      full: process.env.PRICE_FULL,
+      first: process.env.PRICE_FIRST,
+      second: process.env.PRICE_SECOND
+    }
+  },
+  {
+    name: 'Вт',
+    day: day.getDate() + 1,
+    active: false,
+    dayid: day.getDate() + 1 + '' + day.getMonth() + '' + day.getFullYear(),
+    first: 0,
+    second: 0,
+    full: 0,
+    price: {
+      full: process.env.PRICE_FULL,
+      first: process.env.PRICE_FIRST,
+      second: process.env.PRICE_SECOND
+    }
+  },
+  {
+    name: 'Ср',
+    day: day.getDate() + 2,
+    active: false,
+    dayid: day.getDate() + 2 + '' + day.getMonth() + '' + day.getFullYear(),
+    first: 0,
+    second: 0,
+    full: 0,
+    price: {
+      full: process.env.PRICE_FULL,
+      first: process.env.PRICE_FIRST,
+      second: process.env.PRICE_SECOND
+    }
+  },
+  {
+    name: 'Чт',
+    day: day.getDate() + 3,
+    active: false,
+    dayid: day.getDate() + 3 + '' + day.getMonth() + '' + day.getFullYear(),
+    first: 0,
+    second: 0,
+    full: 0,
+    price: {
+      full: process.env.PRICE_FULL,
+      first: process.env.PRICE_FIRST,
+      second: process.env.PRICE_SECOND
+    }
+  },
+  {
+    name: 'Пт',
+    day: day.getDate() + 4,
+    active: false,
+    dayid: day.getDate() + 4 + '' + day.getMonth() + '' + day.getFullYear(),
+    first: 0,
+    second: 0,
+    full: 0,
+    price: {
+      full: process.env.PRICE_FULL,
+      first: process.env.PRICE_FIRST,
+      second: process.env.PRICE_SECOND
+    }
+  }
+]
 
 router.post('/create', ensureAuthenticated, function(req, res) {
   mongoose.model('orders').create(req.body, function(err, order) {
@@ -22,107 +113,19 @@ router.post('/create', ensureAuthenticated, function(req, res) {
   })
 });
 
+router.get('/get-orders', ensureAuthenticated, function(req, res){
+  res.send('In progress')
+});
+
 router.get('/get-week', ensureAuthenticated, function(req, res) {
   mongoose.model('users').findOne({'_id': req.user._id}).populate('orders').exec((err, user) =>{
     if(err) {return res.status(400).send({error: err.message});}
-    function getMonday(d) {
-      d = new Date(d);
-      if(d.getDay() == 5 && d.getHours() > 13) {
-        d.setDate(d.getDate() + 3);
-        var day = d.getDay(),
-            diff = d.getDate() - day + 1;
-        return new Date(d.setDate(diff));
-      } else if (d.getDay() > 5) {
-        d.setDate(d.getDate() + 3);
-        var day = d.getDay(),
-            diff = d.getDate() - day + 1;
-        return new Date(d.setDate(diff));
-      } else {
-        var day = d.getDay(),
-            diff = d.getDate() - day + 1; // adjust when day is sunday
-        return new Date(d.setDate(diff));
-      }
-    }
-    var day = getMonday(new Date());
-    var week = [
-      {
-        name: 'Пн',
-        day: day.getDate(),
-        active: false,
-        dayid: day.getDate() + '' + day.getMonth() + '' + day.getFullYear(),
-        first: 0,
-        second: 0,
-        full: 0,
-        price: {
-          full: process.env.PRICE_FULL,
-          first: process.env.PRICE_FIRST,
-          second: process.env.PRICE_SECOND
-        }
-      },
-      {
-        name: 'Вт',
-        day: day.getDate() + 1,
-        active: false,
-        dayid: day.getDate() + 1 + '' + day.getMonth() + '' + day.getFullYear(),
-        first: 0,
-        second: 0,
-        full: 0,
-        price: {
-          full: process.env.PRICE_FULL,
-          first: process.env.PRICE_FIRST,
-          second: process.env.PRICE_SECOND
-        }
-      },
-      {
-        name: 'Ср',
-        day: day.getDate() + 2,
-        active: false,
-        dayid: day.getDate() + 2 + '' + day.getMonth() + '' + day.getFullYear(),
-        first: 0,
-        second: 0,
-        full: 0,
-        price: {
-          full: process.env.PRICE_FULL,
-          first: process.env.PRICE_FIRST,
-          second: process.env.PRICE_SECOND
-        }
-      },
-      {
-        name: 'Чт',
-        day: day.getDate() + 3,
-        active: false,
-        dayid: day.getDate() + 3 + '' + day.getMonth() + '' + day.getFullYear(),
-        first: 0,
-        second: 0,
-        full: 0,
-        price: {
-          full: process.env.PRICE_FULL,
-          first: process.env.PRICE_FIRST,
-          second: process.env.PRICE_SECOND
-        }
-      },
-      {
-        name: 'Пт',
-        day: day.getDate() + 4,
-        active: false,
-        dayid: day.getDate() + 4 + '' + day.getMonth() + '' + day.getFullYear(),
-        first: 0,
-        second: 0,
-        full: 0,
-        price: {
-          full: process.env.PRICE_FULL,
-          first: process.env.PRICE_FIRST,
-          second: process.env.PRICE_SECOND
-        }
-      }
-    ]
     var curDay = new Date().getDate();
     week.forEach(function (item){
       if (item.day == curDay) {
         item.active = true
       }
     });
-
     week.forEach(function(day) {
       var id = Number(day.dayid);
       for(var i=0; i<user.orders.length; i++) {
