2b295ce74311105f81116d24a226469d9ee33855
kokyhin <kokyhin@gmail.com>
2017-03-20 10:21:10 +0200

    ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,10 @@ module.exports = {
         PORT: 3020,
         PRICE_FIRST: 15,
         PRICE_SECOND: 30,
-        PRICE_FULL: 40
+        PRICE_FULL: 40,
+        APP_URL: 'http://meal.fusionworks.md/',
+        MAIL_ACC: 'fwmeal@gmail.com',
+        MAIL_PASS: 'pass'
       }
     }
   ],
